14f60a2d58eb0796e16ee3af2f9f7b6baaed546e
Michel--Picque Théodore <theodore.michelpicque@gmail.com>
2022-11-02 10:21:46 +0100

    Update parseBio.js

--- Scripts/parseBio.js
+++ Scripts/parseBio.js
@@ -2,27 +2,23 @@ console.log('Début du chargement du fichier')
 
 var XLSX = require('xlsx');
 var workbook = XLSX.readFile('../Data/bio.xlsx');
-var cheptelName = workbook.SheetNames[4];
+var cheptelName = workbook.SheetNames[3];
 var cheptelSheet = workbook.Sheets[cheptelName];
 
 
 
 var cheptel = XLSX.utils.sheet_to_json(cheptelSheet)
 
-console.log(typeof(cheptel))
-
-//cheptel2021 = cheptel.data.filter(x=>x.annee == '2021')
-
-// var cheptel2021 = cheptel.map(x => {
-//     if (x.annee =='2021'){
-//         return x
-//     }
-// })
-
-
 var cheptel2021 = cheptel.filter(function (el) {
     return el.annee == '2021' 
   });
 
+
+
+
+
 console.log(cheptel2021)
+
+
+
 
