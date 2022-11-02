552a811aa7bfa2cfda2da02af0c2c26bdedc7080
clemence17 <clem1707@gmail.com>
2022-11-02 17:05:12 +0100

    update

--- Scripts/dlGaz.js
+++ Scripts/dlGaz.js
@@ -1,13 +1,14 @@
 const http = require('https'); // or 'https' for https:// URLs
 const fs = require('fs');
 
-const file = fs.createWriteStream("file.json");
+const file = fs.createWriteStream("gaz.json");
 const request = http.get("https://opendata.agenceore.fr/explore/dataset/conso-elec-gaz-annuelle-par-secteur-dactivite-agregee-commune/download?format=csv&amptimezone=Europe/Berlin&ampuse_labels_for_header=false", function(response) {
-   response.pipe(file);
+    
+    response.pipe(file);
 
    // after download completed close filestream
    file.on("finish", () => {
        file.close();
-       console.log("Download Completed");
+       console.log("Données du gaz téléchargées");
    });
 });
---
+++ Scripts/transform.js
@@ -0,0 +1,12 @@
+// const jsonfile = require('jsonfile');
+// const path = 'gaz.json';
+
+// jsonfile.readFile(path, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+var csv = require('./file.csv'); //with path
